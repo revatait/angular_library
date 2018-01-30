435530e2d369ce2ed83fb8ba6fb6fb0689e0e35e
Reva Tait <revatait@gmail.com>
2018-01-29 20:04:38 -0500

    scss color scheme implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'the Tait\'s Library';
+  title = 'The Tait\'s Library';
 }
--- src/app/book.ts
+++ src/app/book.ts
@@ -1,4 +1,24 @@
-export class Book {
-	id: number;
-	title: string;
+enum era {
+	'prehistoric',
+	'ancient',
+	'medieval',
+	'renaissance',
+	'modern',
+	'contemporary',
+	'future'
 }
+
+enum medium {
+	'hardcopy',
+	'kindle',
+	'audiobook'
+}
+
+export class Book {
+	public id: number;
+	public title: string;
+	public subtitle?: string;
+	public medium?: medium;
+}
+
+// working on line 20 above: trying to (a) set medium as optional and (b) set it as enum from medium list above
