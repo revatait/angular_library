53e5f7dd046b32266751e4ea8c053a295492744e
Reva Tait <revatait@gmail.com>
2018-01-29 20:27:36 -0500

    todo list in readme

--- src/app/book.ts
+++ src/app/book.ts
@@ -14,11 +14,41 @@ enum medium {
 	'audiobook'
 }
 
+enum category {
+	'fiction',
+	'nonfiction',
+	'kids fiction',
+	'kids nonfiction',
+	'baby'
+}
+
+enum status {
+	'shelved',
+	'donated',
+	'borrowed',
+	'lent',
+	'sold',
+	'recommended'
+}
+
+enum position {
+	'currently reading',
+	'read',
+	'to read',
+	'wishlist',
+	'abandoned',
+	'reference'
+}
+
 export class Book {
 	public id: number;
 	public title: string;
 	public subtitle?: string;
 	public medium?: medium;
+	public category?: category;
+	public era?: era;
+	public status?: status;
+	public position?: position;
 }
 
 // working on line 20 above: trying to (a) set medium as optional and (b) set it as enum from medium list above
